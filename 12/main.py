1b29c254d6766f89bbb99e9646ae901d75139c13
Matteo De Wint <matteo@gorilla.co>
2022-12-12 22:18:36 +0100

    Day 12: faster solution using breadth-first search

--- 12/main.py
+++ 12/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 from pathlib import Path
 
 Pos = tuple[int, int]
@@ -5,7 +6,7 @@ Map = dict[Pos, int]
 
 
 def parse_map(text: str) -> tuple[Map, Pos, Pos]:
-    H: Map = {}
+    hmap: Map = {}
     start = end = (-1, -1)
 
     for y, line in enumerate(text.splitlines()):
@@ -17,68 +18,40 @@ def parse_map(text: str) -> tuple[Map, Pos, Pos]:
             elif char == "E":
                 end = pos
                 char = "z"
-            H[pos] = ord(char) - ord("a")
+            hmap[pos] = ord(char) - ord("a")
 
-    return H, start, end
+    return hmap, start, end
 
 
-def shortest_path(H: Map, start: Pos, end: Pos) -> list[Pos]:
-    dist = {pos: len(H) for pos in H}
-    dist[start] = 0
-
-    todo = set(H)
-    prev: dict[Pos, Pos] = {}
+def shortest_path(hmap: Map, start: list[Pos], end: Pos) -> int:
+    todo = deque((pos, 0) for pos in start)
+    seen = set()
 
     while todo:
-        _, u = min((dist[u], u) for u in todo)
-        if u == end:
-            break
-        todo.remove(u)
+        pos, dist = todo.popleft()
+        if pos in seen:
+            continue
+        seen.add(pos)
 
-        x, y = u
-        neighbours = ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1))
-        reachable = (v for v in neighbours if v in H and H[v] <= H[u] + 1)
-        for v in todo.intersection(reachable):
-            alt = dist[u] + 1
-            if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = u
+        if pos == end:
+            return dist
 
-    path = []
-    pos: Pos | None = end
-    if pos in prev or pos == start:
-        while pos:
-            path.append(pos)
-            pos = prev.get(pos)
+        x, y = pos
+        for new in (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1):
+            if new in hmap and hmap[new] <= hmap[pos] + 1:
+                todo.append((new, dist + 1))
 
-    return list(reversed(path))
-
-
-def print_path(H: Map, path: list[Pos]) -> None:
-    w = max(x for x, _ in H) + 1
-    h = max(y for _, y in H) + 1
-
-    grid = [["." for _ in range(w)] for _ in range(h)]
-    for x, y in path:
-        grid[y][x] = "#"
-
-    for row in grid:
-        print("".join(row))
+    raise Exception("No path found")
 
 
 def main():
     text = Path("input.txt").read_text()
-    H, start, end = parse_map(text)
+    hmap, start, end = parse_map(text)
 
-    path = shortest_path(H, start, end)
-    print("Part 1:", len(path) - 1)
-    # print_path(H, path)
+    print("Part 1:", shortest_path(hmap, [start], end))
 
-    # HACK: The low points on the left edge are good candidates,
-    # but all others are pits we can't escape.
-    lowest = (pos for pos, h in H.items() if h == 0 and pos[0] == 0)
-    paths = (shortest_path(H, start, end) for start in lowest)
-    print("Part 2:", min(len(p) for p in paths if p) - 1)
+    lowest = [pos for pos, h in hmap.items() if h == 0]
+    print("Part 2:", shortest_path(hmap, lowest, end))
 
 
 if __name__ == "__main__":
