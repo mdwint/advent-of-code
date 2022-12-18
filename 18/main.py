8c26c609a3d5b2a28190647d83428545fe4cc6f8
Matteo De Wint <matteo@gorilla.co>
2022-12-18 11:54:30 +0100

    Day 18: refactor

--- 18/main.py
+++ 18/main.py
@@ -5,28 +5,12 @@ from pathlib import Path
 DEBUG = False
 
 Pos = tuple[int, int, int]
-Side = tuple[Pos, Pos]
 
 
 def neighbours(pos: Pos) -> list[Pos]:
     x, y, z = pos
     deltas = ((-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1))
     return [(x + dx, y + dy, z + dz) for dx, dy, dz in deltas]
-
-
-def compute_areas(points: set[Pos]) -> tuple[int, int]:
-    free_sides: set[Side] = set()
-    exterior_sides: set[Side] = set()
-
-    for pos in points:
-        for other in neighbours(pos):
-            if other not in points:
-                side = (pos, other)
-                free_sides.add(side)
-                if reaches_exterior(other, points):
-                    exterior_sides.add(side)
-
-    return len(free_sides), len(exterior_sides)
 
 
 def reaches_exterior(start: Pos, points: set[Pos], max_dist: int = 20) -> bool:
@@ -60,7 +44,16 @@ def main() -> None:
     scan = Path(args.input).read_text().splitlines()
     points: set[Pos] = {tuple(int(x) for x in line.split(",")) for line in scan}  # type: ignore
 
-    free_area, exterior_area = compute_areas(points)
+    free_area = 0
+    exterior_area = 0
+
+    for pos in points:
+        for other in neighbours(pos):
+            if other not in points:
+                free_area += 1
+                if reaches_exterior(other, points):
+                    exterior_area += 1
+
     print("Part 1:", free_area)
     print("Part 2:", exterior_area)
 
