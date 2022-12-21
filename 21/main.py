cd2946dbcce0d69e79ebe23bd4b945f2282f72f8
Matteo De Wint <matteo@gorilla.co>
2022-12-21 12:42:08 +0100

    Day 21: part 2

--- 21/main.py
+++ 21/main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Any, Callable
+from functools import lru_cache
+from typing import Callable
 
 from aoc import read_input
 
@@ -31,42 +32,91 @@ class Graph:
     tasks: list[Task]
 
 
-def compute(graph: Graph) -> int:
-    tasks = {t.id: t for t in graph.tasks}
+class Solver:
+    def __init__(self, graph: Graph):
+        self.tasks = {t.id: t for t in graph.tasks}
+        self.cache: dict[str, int] = {}
+        self.seen: set[str] = set()
 
-    cache: dict[str, Any] = {}
-    seen: set[str] = set()
+    def compute(self, task_id: str = "root") -> int:
+        return self._compute_task(self.tasks[task_id])
 
-    def resolve(value: Value):
+    def _compute_task(self, task: Task) -> int:
+        inputs = [self._resolve(value) for value in task.inputs]
+        output = task.op(*inputs)
+        self.cache[task.id] = output
+        return output
+
+    def _resolve(self, value: Value) -> int:
         if isinstance(value, LiteralValue):
             return value.value
 
         if isinstance(value, OutputValue):
             tid = value.task_id
-            if tid in cache:
-                return cache[tid]
-            if tid in seen:
+            if tid in self.cache:
+                return self.cache[tid]
+            if tid in self.seen:
                 raise GraphIsCyclic(tid)
-            seen.add(tid)
-            return compute_task(tasks[tid])
+            self.seen.add(tid)
+            return self._compute_task(self.tasks[tid])
 
         raise NotImplementedError(value)
 
-    def compute_task(task: Task) -> int:
-        inputs = [resolve(value) for value in task.inputs]
-        output = task.op(*inputs)
-        print(task.id, inputs, "->", output)
-        cache[task.id] = output
-        return output
+    def find_human_input(self) -> int:
+        root = self.tasks["root"]
+        humn = self.tasks["humn"]
 
-    return compute_task(tasks["root"])
+        @lru_cache(maxsize=None)
+        def is_reachable(dst_id: str, src_id: str) -> bool:
+            if dst_id == src_id:
+                return True
+
+            t = self.tasks[dst_id]
+            return any(
+                is_reachable(inp.task_id, src_id)
+                for inp in t.inputs
+                if isinstance(inp, OutputValue)
+            )
+
+        def split(root: Task) -> tuple[OutputValue, OutputValue]:
+            ours = next(inp for inp in root.inputs if is_reachable(inp.task_id, humn.id))  # type: ignore
+            theirs = next(inp for inp in root.inputs if inp is not ours)
+            return ours, theirs  # type: ignore
+
+        ours, theirs = split(root)
+        target = self.compute(theirs.task_id)
+
+        while ours.task_id != humn.id:
+            t = self.tasks[ours.task_id]
+            ours, theirs = split(t)
+            ours_lhs = t.inputs.index(ours) == 0
+
+            x = self.compute(theirs.task_id)
+
+            if t.op is OPS["+"]:
+                target -= x
+            elif t.op is OPS["*"]:
+                target //= x
+            elif t.op is OPS["-"]:
+                target = target + x if ours_lhs else x - target
+            elif t.op is OPS["/"]:
+                target = target * x if ours_lhs else x // target
+
+        # Verify result:
+        humn.inputs[0].value = target  # type: ignore
+        root.op = lambda a, b: int(a == b)
+        self.cache.clear()
+        self.seen.clear()
+        assert self.compute(root.id) == 1
+
+        return target
 
 
 class GraphIsCyclic(Exception):
     pass
 
 
-ops = {
+OPS = {
     "+": lambda a, b: a + b,
     "-": lambda a, b: a - b,
     "*": lambda a, b: a * b,
@@ -82,13 +132,13 @@ def parse_graph(text: str) -> Graph:
             x = int(expr)
         except ValueError:
             a, op, b = expr.split(" ", 2)
-            t = Task(tid, ops[op], inputs=[OutputValue(dep) for dep in (a, b)])
+            t = Task(tid, OPS[op], inputs=[OutputValue(dep) for dep in (a, b)])
         else:
             t = Task(tid, op=lambda x: x, inputs=[LiteralValue(x)])
         g.tasks.append(t)
     return g
 
 
-g = parse_graph(read_input())
-print("Part 1:", compute(g))
-# print("Part 2:", 0)
+s = Solver(parse_graph(read_input()))
+print("Part 1:", s.compute())
+print("Part 2:", s.find_human_input())
